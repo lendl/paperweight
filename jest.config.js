93e8018f8109b03f8bb91741a9166c4b2b5f570f
wslyvh <wslyvh@users.noreply.github.com>
2026-07-13 15:22:43 +0200

    vitest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/analysis/"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
